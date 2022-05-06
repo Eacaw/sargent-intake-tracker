7769c3a99a67cfd4fcd55a666a1ba8457c60e9af
Eacaw <davidpinchen@gmail.com>
2022-05-06 15:38:36 +0100

    Update navigation

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,11 +2,17 @@ import { getAuth, signOut } from "firebase/auth";
 import {
   MDBBtn,
   MDBContainer,
+  MDBDropdown,
+  MDBDropdownItem,
+  MDBDropdownLink,
+  MDBDropdownMenu,
+  MDBDropdownToggle,
   MDBNavbar,
   MDBNavbarBrand,
 } from "mdb-react-ui-kit";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
   const auth = getAuth();
@@ -20,7 +26,29 @@ function Navbar() {
     <MDBNavbar fixed expand="lg" light bgColor="light">
       <MDBContainer fluid>
         <MDBNavbarBrand href="#">Sargent Intake Tracker</MDBNavbarBrand>
-        {user ? <MDBBtn onClick={logout}>Log out</MDBBtn> : null}
+        {user ? (
+          <MDBDropdown
+            group
+            className="shadow-0"
+            style={{ textAlign: "center" }}
+          >
+            <MDBDropdownToggle
+              color="light"
+              style={{ border: "1px solid #e0e0e0", borderRadius: "20px" }}
+            ></MDBDropdownToggle>
+            <MDBDropdownMenu style={{ textAlign: "right" }}>
+              <MDBDropdownItem>
+                <MDBDropdownLink href="/feed">Feed</MDBDropdownLink>
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                <MDBDropdownLink href="/add-new-food">Add food</MDBDropdownLink>
+              </MDBDropdownItem>
+              <MDBDropdownItem>
+                <MDBDropdownLink onClick={logout}>Log out</MDBDropdownLink>
+              </MDBDropdownItem>
+            </MDBDropdownMenu>
+          </MDBDropdown>
+        ) : null}
       </MDBContainer>
     </MDBNavbar>
   );
